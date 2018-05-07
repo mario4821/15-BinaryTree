580a98e982b49cb878d3527de57638e2c17ba76a
Mario Flores Jr <m.flores4821@gmail.com>
2018-05-07 01:00:27 -0700

    saved some work

---
+++ src/__test__/binary-tree.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import BinaryTree from '../lib/binary-tree';
+import Node from '../lib/node';
+
+const one = new Node(1);
+const two = new Node(2);
+const three = new Node(3);
+const four = new Node(4);
+const five = new Node(5);
+const six = new Node(6);
+const seven = new Node(7);
+const eight = new Node(8);
+const nine = new Node(9);
